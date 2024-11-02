d534e7ed149de3ead6915ddeca5cce552eb442b8
nsm-kil3r <nsm-kil3r@nsm.nsm>
2024-11-02 01:43:25 +0100

    Updated face recognition code with encoding function

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,30 +1,35 @@
+# EncodeGenerator.py
 import os
 import cv2
 import face_recognition
 import pickle
 
-folderModpath ='img/Modes'
-pathList=os.listdir(folderModpath)
-imgList=[]
+def generate_encoded_images():
+    folderModpath = 'img/Modes'
+    pathList = os.listdir(folderModpath)
+    imgList = []
+    modelsIds = []
 
-modelsIds=[]
+    for path in pathList:
+        imgList.append(cv2.imread(os.path.join(folderModpath, path)))
+        modelsIds.append(os.path.splitext(path)[0])
 
+    def findEncodings(imagesList):
+        encodeList = []
+        for img in imagesList:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
 
-for path in pathList:
-    imgList.append(cv2.imread(os.path.join(folderModpath,path)))
-    modelsIds.append(os.path.splitext(path)[0])
+    print("Encoding started")
+    encodeListKnown = findEncodings(imgList)
+    encodeListKnownWithIds = [encodeListKnown, modelsIds]
+    print("Encoding finished")
+    
+    file = open("EncodedImages.p", 'wb')
+    pickle.dump(encodeListKnownWithIds, file)
+    file.close()
 
-def findEncodings(imagesList):
-    encodeList=[]
-    for img in imagesList:
-        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encode =face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
-print("encoding started")
-encodeListKnown =findEncodings(imgList)
-encodeListKnownthWithIds=[encodeListKnown,modelsIds]
-print("encoding finished")
-file =open("EncodedImages.p",'wb')
-pickle.dump(encodeListKnownthWithIds,file)
-file.close()
+if __name__ == "__main__":
+    generate_encoded_images()
--- main.py
+++ main.py
@@ -3,37 +3,45 @@ import cv2
 import face_recognition
 import numpy as np
 import cvzone 
-cap=cv2.VideoCapture(0)
-#import the encoded images
-file = open('EncodedImages.p','rb')
-encodeListKnownWithIds=pickle.load(file)
-file.close()
-encodeListKnown,modelsIds = encodeListKnownWithIds
+import EncodeGenerator
 
-#camera open
+
+EncodeGenerator.generate_encoded_images()  
+
+cap = cv2.VideoCapture(0)
+# Import the encoded images
+file = open('EncodedImages.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, modelsIds = encodeListKnownWithIds
+
+# Camera open
 while True:
-    grabbed,frame = cap.read()
-    frameS=cv2.resize(frame,(0,0),None,0.25,0.25)
-    frameS = cv2.cvtColor(frameS,cv2.COLOR_BGR2RGB)
-    faceCurntFrame= face_recognition.face_locations(frameS)
-    encodeCurntFrame=face_recognition.face_encodings(frameS,faceCurntFrame)
+    grabbed, frame = cap.read()
+    frameS = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    frameS = cv2.cvtColor(frameS, cv2.COLOR_BGR2RGB)
+    faceCurntFrame = face_recognition.face_locations(frameS)
+    encodeCurntFrame = face_recognition.face_encodings(frameS, faceCurntFrame)
     
-    for encodeFace,faceLoc in zip(encodeCurntFrame,faceCurntFrame):
-        match = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDistance =face_recognition.face_distance(encodeListKnown,encodeFace)
-        # print("matches",match)
-        # print("faceDistance",faceDistance)
-        matchIndex =np.argmin(faceDistance)
-        # print("match index is:",matchIndex)
-        top,right,bottom,left=faceLoc
-        top, right, bottom, left = top*4,right*4,bottom*4,left*4
-        cvzone.cornerRect(frame, (left, top, right - left, bottom - top), colorR=(0, 255, 0),rt=0 )
+    for encodeFace, faceLoc in zip(encodeCurntFrame, faceCurntFrame):
+        match = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDistance = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("matches", match)
+        # print("faceDistance", faceDistance)
+        matchIndex = np.argmin(faceDistance)
+        # print("match index is:", matchIndex)
+        top, right, bottom, left = faceLoc
+        top, right, bottom, left = top * 4, right * 4, bottom * 4, left * 4
+        cvzone.cornerRect(frame, (left, top, right - left, bottom - top), colorR=(0, 255, 0), rt=0)
         if match[matchIndex]:
-            print("known face detectedand it's id is:"+modelsIds[matchIndex])
+            print("known face detected and it's id is:" + modelsIds[matchIndex])
         else:
             print("the face is not in the database")
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    cv2.imshow("face",frame)
+    
+    cv2.imshow("face", frame)
+
 cap.release()
 cv2.destroyAllWindows()
