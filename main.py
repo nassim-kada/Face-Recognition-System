be6fde8b72135a15a829f56e6a9004b29533267d
nsm-kil3r <nsm-kil3r@nsm.nsm>
2024-11-02 01:06:39 +0100

    changed-code

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-# import cv2
-# IMG_PATH="/home/nsm-kil3r/Desktop/wall/1.jpg"
-# image=cv2.imread(IMG_PATH)
-
-# cv2.imshow('CLOWN',image)
-# cv2.waitKey(0)
-
-# import cv2
-# video =cv2.VideoCapture(0) # 0 for local webcam 1 external webcam 2 external webcam 2
-# while True:
-#     grabbed,frame =video.read()
-#     frame=cv2.resize(frame, (800, 500))
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#     cv2.imshow('video',frame)
-# video.release()
-# cv2.destroyAllWindows()
-
-# import numpy as np
-# import cv2 
-# image= cv2.imread("/home/nsm-kil3r/Desktop/wall/1.jpg")
-# cv2.imshow('AI',image)
-# cv2.waitKey(0)
-
 import pickle
 import cv2
 import face_recognition
 import numpy as np
+import cvzone 
 cap=cv2.VideoCapture(0)
 #import the encoded images
 file = open('EncodedImages.p','rb')
@@ -48,11 +25,11 @@ while True:
         # print("faceDistance",faceDistance)
         matchIndex =np.argmin(faceDistance)
         # print("match index is:",matchIndex)
+        top,right,bottom,left=faceLoc
+        top, right, bottom, left = top*4,right*4,bottom*4,left*4
+        cvzone.cornerRect(frame, (left, top, right - left, bottom - top), colorR=(0, 255, 0),rt=0 )
         if match[matchIndex]:
             print("known face detectedand it's id is:"+modelsIds[matchIndex])
-            top,right,bottom,left=faceLoc
-            top, right, bottom, left = top*4,right*4,bottom*4,left*4
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 1)
         else:
             print("the face is not in the database")
     if cv2.waitKey(1) & 0xFF == ord('q'):
